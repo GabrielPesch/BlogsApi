70f1a92609e62a801bbab85c9af5527be6caca81
Gabriel Pesch <gkpesch@gmail.com>
2022-07-14 14:56:37 -0300

    chore(test Login): Utils que deveriam ter sido encaminhados no ultimo commit
    As utils "mocha/utils/constants.js" e mocha/utils/utils.js deveriam ter sido encaminhadas juntos com o ultimo commit

---
+++ mocha/utils/constants.js
@@ -0,0 +1,5 @@
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imxld2lzaGFtaWx0b25AZ21haWwuY29tIiwicGFzc3dvcmQiOiIxMjM0NTYifQ.A5O55FigiHApipLgVnVwQg9sg9dNa-740TIjA0uD10E';
+
+module.exports = {
+  TOKEN
+}
