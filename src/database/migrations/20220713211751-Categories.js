69d23fbbd42744078d9708dafab182ac7dc3589e
Gabriel Pesch <gkpesch@gmail.com>
2022-07-17 16:34:24 -0300

    fix(models): Model categories referencia a tabela correta agora.

--- src/database/migrations/20220713211751-Categories.js
+++ src/database/migrations/20220713211751-Categories.js
@@ -12,7 +12,8 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
     });
   },
