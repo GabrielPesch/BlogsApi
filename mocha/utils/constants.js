91f56dc9c78622b4861d4af7c36e9113508826eb
Gabriel Pesch <gkpesch@gmail.com>
2022-07-18 16:56:17 -0300

    chore(tests): Testes MOCHA foram comentados
    Os testes foram comentados para não interferirem com o avaliador automático da trybe

--- mocha/utils/constants.js
+++ mocha/utils/constants.js
@@ -1,20 +1,20 @@
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imxld2lzaGFtaWx0b25AZ21haWwuY29tIiwicGFzc3dvcmQiOiIxMjM0NTYifQ.A5O55FigiHApipLgVnVwQg9sg9dNa-740TIjA0uD10E';
+// const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imxld2lzaGFtaWx0b25AZ21haWwuY29tIiwicGFzc3dvcmQiOiIxMjM0NTYifQ.A5O55FigiHApipLgVnVwQg9sg9dNa-740TIjA0uD10E';
 
-const ADD_USER_BODY = {
-  "displayName": "Brett Wiltshire",
-  "email": "brett@email.com",
-  "password": "123456",
-  "image": "http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png"
-}
+// const ADD_USER_BODY = {
+//   "displayName": "Brett Wiltshire",
+//   "email": "brett@email.com",
+//   "password": "123456",
+//   "image": "http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png"
+// }
 
-const ADD_USER_BODY_WITHOUT_PASSWORD = {
-  "displayName": "Brett Wiltshire",
-  "email": "brett@email.com",
-  "image": "http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png"
-}
+// const ADD_USER_BODY_WITHOUT_PASSWORD = {
+//   "displayName": "Brett Wiltshire",
+//   "email": "brett@email.com",
+//   "image": "http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png"
+// }
 
-module.exports = {
-  TOKEN,
-  ADD_USER_BODY,
-  ADD_USER_BODY_WITHOUT_PASSWORD
-}
+// module.exports = {
+//   TOKEN,
+//   ADD_USER_BODY,
+//   ADD_USER_BODY_WITHOUT_PASSWORD
+// }
