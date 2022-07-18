86d0259d73df4f510bb3272d1757c3d5aa4029e1
Gabriel Pesch <gkpesch@gmail.com>
2022-07-18 16:53:44 -0300

    test(POST): Criados os testes unitários para a rota POST /post
    Criados os testes unitários para a rota, com exceção da função postService.create

--- mocha/unit/controllers/categoriesController.test.js
+++ mocha/unit/controllers/categoriesController.test.js
@@ -1,66 +1,66 @@
-// const sinon = require('sinon');
-// const chai = require('chai');
-// const chaiAsPromised = require('chai-as-promised');
+const sinon = require('sinon');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 
-// const { makeRes } = require('../../utils/mocks');
-// const authorizationMiddleware = require('../../../src/middlewares/authorizationMiddleware');
-// const categoriesService = require('../../../src/services/categoriesService');
-// const categoriesController = require('../../../src/controllers/categoriesController');
+const { makeRes } = require('../../utils/mocks');
+const authorizationMiddleware = require('../../../src/middlewares/authorizationMiddleware');
+const categoriesService = require('../../../src/services/categoriesService');
+const categoriesController = require('../../../src/controllers/categoriesController');
 
-// chai.use(chaiAsPromised);
+chai.use(chaiAsPromised);
 
-// describe('controllers/categoriesController', () => {
-//   beforeEach(sinon.restore);
+describe('controllers/categoriesController', () => {
+  beforeEach(sinon.restore);
 
-//   describe('add', () => {
-//     it('Deve disparar um erro se categoriesService.validateBodyAdd disparar um erro', () => {
-//       sinon.stub(categoriesService, 'validateBodyAdd').rejects();
-//       sinon.stub(authorizationMiddleware, 'validate').resolves();
-//       return chai.expect(categoriesController.add({}, {})).to.eventually.be.rejected;
-//     });
+  describe('add', () => {
+    it('Deve disparar um erro se categoriesService.validateBodyAdd disparar um erro', () => {
+      sinon.stub(categoriesService, 'validateBodyAdd').rejects();
+      sinon.stub(authorizationMiddleware, 'validate').resolves();
+      return chai.expect(categoriesController.add({}, {})).to.eventually.be.rejected;
+    });
 
-//     it('Deve disparar um erro se authorizationMiddleware.validate disparar um erro', () => {
-//       sinon.stub(categoriesService, 'validateBodyAdd').resolves();
-//       sinon.stub(authorizationMiddleware, 'validate').rejects();
-//       return chai.expect(categoriesController.add({}, {})).to.eventually.be.rejected;
-//     });
+    it('Deve disparar um erro se authorizationMiddleware.validate disparar um erro', () => {
+      sinon.stub(categoriesService, 'validateBodyAdd').resolves();
+      sinon.stub(authorizationMiddleware, 'validate').rejects();
+      return chai.expect(categoriesController.add({}, {})).to.eventually.be.rejected;
+    });
 
-//     it('Deve disparar um erro se categoriesService.findOrCreate disparar um erro', () => {
-//       sinon.stub(categoriesService, 'validateBodyAdd').resolves();
-//       sinon.stub(authorizationMiddleware, 'validate').resolves();
-//       sinon.stub(categoriesService, 'findOrCreate').rejects
-//       return chai.expect(categoriesController.add({}, {})).to.eventually.be.rejected;
-//     });
+    it('Deve disparar um erro se categoriesService.findOrCreate disparar um erro', () => {
+      sinon.stub(categoriesService, 'validateBodyAdd').resolves();
+      sinon.stub(authorizationMiddleware, 'validate').resolves();
+      sinon.stub(categoriesService, 'findOrCreate').rejects
+      return chai.expect(categoriesController.add({}, {})).to.eventually.be.rejected;
+    });
 
-//     it('Deve chamar o status "201" e retornar o token', async () => {
-//       sinon.stub(categoriesService, 'validateBodyAdd').resolves();
-//       sinon.stub(authorizationMiddleware, 'validate').resolves();
-//       sinon.stub(categoriesService, 'findOrCreate').resolves({});
-//       const res = makeRes();
-//       await categoriesController.add({body: {name: ''}, headers: {authorization: ''}}, res);
-//       chai.expect(res.status.getCall(0).args[0]).to.deep.equal(201);
-//       return chai.expect(res.json.getCall(0).args[0]).to.deep.equal({});
-//     });
-//   });
+    it('Deve chamar o status "201" e retornar o token', async () => {
+      sinon.stub(categoriesService, 'validateBodyAdd').resolves();
+      sinon.stub(authorizationMiddleware, 'validate').resolves();
+      sinon.stub(categoriesService, 'findOrCreate').resolves({});
+      const res = makeRes();
+      await categoriesController.add({body: {name: ''}, headers: {authorization: ''}}, res);
+      chai.expect(res.status.getCall(0).args[0]).to.deep.equal(201);
+      return chai.expect(res.json.getCall(0).args[0]).to.deep.equal({});
+    });
+  });
 
-//   describe.only('getAll', () => {
-//     it('Deve disparar um erro se authorizationMiddleware.validate disparar um erro', () => {
-//       sinon.stub(authorizationMiddleware, 'validate').rejects();
-//       return chai.expect(categoriesController.getAll({}, {})).to.eventually.be.rejected;
-//     });
+  describe('getAll', () => {
+    it('Deve disparar um erro se authorizationMiddleware.validate disparar um erro', () => {
+      sinon.stub(authorizationMiddleware, 'validate').rejects();
+      return chai.expect(categoriesController.getAll({}, {})).to.eventually.be.rejected;
+    });
 
-//     it('Deve disparar um erro se categoriesService.getAll disparar um erro', () => {
-//       sinon.stub(authorizationMiddleware, 'validate').resolves();
-//       sinon.stub(categoriesService, 'getAll').rejects();
-//       return chai.expect(categoriesController.getAll({}, {})).to.eventually.be.rejected;
-//     });
+    it('Deve disparar um erro se categoriesService.getAll disparar um erro', () => {
+      sinon.stub(authorizationMiddleware, 'validate').resolves();
+      sinon.stub(categoriesService, 'getAll').rejects();
+      return chai.expect(categoriesController.getAll({}, {})).to.eventually.be.rejected;
+    });
 
-//     it('Deve retornar a lista de categorias', async () => {
-//       sinon.stub(authorizationMiddleware, 'validate').resolves();
-//       sinon.stub(categoriesService, 'getAll').resolves([]);
-//       const res = makeRes();
-//       await categoriesController.getAll({headers: {authorization: ''}}, res);
-//       return chai.expect(res.json.getCall(0).args[0]).to.deep.equal([]);
-//     });
-//   });
-// });
+    it('Deve retornar a lista de categorias', async () => {
+      sinon.stub(authorizationMiddleware, 'validate').resolves();
+      sinon.stub(categoriesService, 'getAll').resolves([]);
+      const res = makeRes();
+      await categoriesController.getAll({headers: {authorization: ''}}, res);
+      return chai.expect(res.json.getCall(0).args[0]).to.deep.equal([]);
+    });
+  });
+});
--- mocha/unit/services/categoriesService.test.js
+++ mocha/unit/services/categoriesService.test.js
@@ -1,28 +1,57 @@
-// const sinon = require('sinon');
-// const chai = require('chai');
-// const chaiAsPromised = require('chai-as-promised');
-// const { Category } = require('../../../src/database/models');
-// const categoriesService = require('../../../src/services/categoriesService');
+const sinon = require('sinon');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const { Category } = require('../../../src/database/models');
+const categoriesService = require('../../../src/services/categoriesService');
 
-// chai.use(chaiAsPromised);
+chai.use(chaiAsPromised);
 
-// describe('services/categoriesService', () => {
-//   beforeEach(sinon.restore);
+describe('services/categoriesService', () => {
+  beforeEach(sinon.restore);
 
-//   describe('findOrCreate', () => {
-//     it('Deve disparar um erro caso Category.findOrCreate também dispare', () => {
-//       sinon.stub(Category, 'findOrCreate').rejects();
-//       return chai.expect(Category.findOrCreate('')).to.eventually.be.rejected;
-//     });
+  describe('findOrCreate', () => {
+    it('Deve disparar um erro caso Category.findOrCreate também dispare', () => {
+      sinon.stub(Category, 'findOrCreate').rejects();
+      return chai.expect(Category.findOrCreate('')).to.eventually.be.rejected;
+    });
 
-//     it('Deve disparar um "Category already registered" caso Category.findOrCreate retorne created como False', () => {
-//       sinon.stub(Category, 'findOrCreate').resolves([{}, false]);
-//       return chai.expect(categoriesService.findOrCreate('')).to.eventually.be.rejectedWith('Category already registered');
-//     });
+    it('Deve disparar um "Category already registered" caso Category.findOrCreate retorne created como False', () => {
+      sinon.stub(Category, 'findOrCreate').resolves([{}, false]);
+      return chai.expect(categoriesService.findOrCreate('')).to.eventually.be.rejectedWith('Category already registered');
+    });
 
-//     it('Deve retornar um usuário sem a senha caso User.findOrCreate retorne created como true', () => {
-//       sinon.stub(Category, 'findOrCreate').resolves([{}, {}]);;
-//       return chai.expect(categoriesService.findOrCreate('')).to.eventually.be.deep.equal({});
-//     });
-//   });
-// });
+    it('Deve retornar um usuário sem a senha caso User.findOrCreate retorne created como true', () => {
+      sinon.stub(Category, 'findOrCreate').resolves([{}, {}]);;
+      return chai.expect(categoriesService.findOrCreate('')).to.eventually.be.deep.equal({});
+    });
+  });
+
+  describe('getAll', () => {
+    it('Deve disparar um erro caso Category.findAll também dispare', () => {
+      sinon.stub(Category, 'findAll').rejects();
+      return chai.expect(categoriesService.getAll()).to.eventually.be.rejected;
+    });
+
+    it('Deve retornar um array caso Category.findAll tenha sucesso', () => {
+      sinon.stub(Category, 'findAll').resolves([{}]);
+      return chai.expect(categoriesService.getAll()).to.eventually.be.deep.equal([{}]);
+    });
+  });
+
+  describe('checkExistsById', () => {
+    it('Deve disparar um erro caso Category.findAndCountALL também dispare', () => {
+      sinon.stub(Category, 'findAndCountAll').rejects();
+      return chai.expect(categoriesService.checkExistsById()).to.eventually.be.rejected;
+    });
+
+    it('Deve disparar um erro CategoryNotFound caso Category.findAndCountAll não encontre todos os IDs', () => {
+      sinon.stub(Category, 'findAndCountAll').resolves({count: 1});
+      return chai.expect(categoriesService.checkExistsById([1, 2])).to.eventually.be.rejectedWith('"categoryIds" not found');
+    });
+
+    it('Deve retornar undefined caso sejam  encontrados todos os ids', () => {
+      sinon.stub(Category, 'findAndCountAll').resolves({count: 3});
+      return chai.expect(categoriesService.checkExistsById([1, 2, 3])).to.eventually.be.undefined;
+    })
+  });
+});
