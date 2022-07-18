cf6cde67226bc046081778529ce90f5ea196dde2
Gabriel Pesch <gkpesch@gmail.com>
2022-07-18 19:22:42 -0300

    test(POST): Criado os testes unitários da rota GET /post/:id
    Os testes unitários para a rota GET /post/:id foram criados

--- mocha/unit/middlewares/validator.test.js
+++ mocha/unit/middlewares/validator.test.js
@@ -1,26 +1,26 @@
-// const chai = require('chai');
-// const sinon = require('sinon');
-// const chaiAsPromised = require('chai-as-promised');
-// const { runSchema } = require('../../../src/middlewares/validator');
-// const Joi = require('joi');
+const chai = require('chai');
+const sinon = require('sinon');
+const chaiAsPromised = require('chai-as-promised');
+const { runSchema } = require('../../../src/middlewares/validator');
+const Joi = require('joi');
 
-// chai.use(chaiAsPromised);
+chai.use(chaiAsPromised);
 
-// const schema = Joi.object();
+const schema = Joi.object();
 
-// describe('validators', () => {
-//   beforeEach(sinon.restore);
+describe('validators', () => {
+  beforeEach(sinon.restore);
 
-//   describe('runSchema', () => {
-//     it('deve disparar um erro se o schema disparar um erro', () => {
-//       sinon.stub(schema, 'validateAsync').rejects();
-//       return chai.expect(runSchema(schema)()).to.eventually.be.rejected;
-//     });
+  describe('runSchema', () => {
+    it('deve disparar um erro se o schema disparar um erro', () => {
+      sinon.stub(schema, 'validateAsync').rejects();
+      return chai.expect(runSchema(schema)()).to.eventually.be.rejected;
+    });
 
-//     it('deve retornar algo se a validação for bem sucedida', () => {
-//       sinon.stub(schema, 'validateAsync').resolves();
-//       return chai.expect(runSchema(schema)())
-//       .to.eventually.be.undefined;
-//     });
-//   });
-// });
+    it('deve retornar algo se a validação for bem sucedida', () => {
+      sinon.stub(schema, 'validateAsync').resolves();
+      return chai.expect(runSchema(schema)())
+      .to.eventually.be.undefined;
+    });
+  });
+});
